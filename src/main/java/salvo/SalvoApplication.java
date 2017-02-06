557eb21fb09dedc6351a49bb28071e95925beb77
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2017-02-06 12:35:06 +0100

    Make index.html main page

--- src/main/java/salvo/SalvoApplication.java
+++ src/main/java/salvo/SalvoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -21,6 +23,14 @@ public class SalvoApplication {
 		SpringApplication.run(SalvoApplication.class, args);
 	}
 
+	@Configuration
+	class MvcConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/").setViewName("index.html");
+		}
+	}
 
 	@Configuration
 	public class DatabaseConfig {
